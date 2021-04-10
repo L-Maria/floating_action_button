4b77fb22b3f391eac4d47ddf1078f87fd0d70533
DESKTOP-HVDBARJ\ml <ma27lazar@gmail.com>
2021-04-10 22:29:23 +0300

    new map

--- app/src/main/java/com/example/buton/MainActivity.kt
+++ app/src/main/java/com/example/buton/MainActivity.kt
@@ -11,6 +11,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         bottomNavigationView.background = null
         bottomNavigationView.menu.getItem (1).isEnabled=false
-//        bottomNavigationView.menu.getItem (0).setOnMenuItemClickListener({startActivity(Intent(this, Harta_1::class.java)) }
+
+
+
+//        bottomNavigationView.setOnNavigationItemSelectedListener{
+//            when (it.itemId){
+//                R.id.miMap -> startActivity(Intent(Harta_1))
+//                R.id.miMap-> setOnClickListener({startActivity(Intent(this, Harta_1::class.java)) })
+//                else ->  startActivity(MainActivity)
+//            }
+//            true
+//        }
+
+
+
+//        class CustomViewHolder(val view: View): RecyclerView.ViewHolder (view){
+//             init{
+//                  view.setOnClickListener{
+//                      val intent =Intent(view.context, Harta_1:class.java)
+//                      view.context.startActivity(intent)
+//                  }
+//             }
+//        }
+
+
+//        miMap.setOnClickListener({startActivity(Intent(this, Harta_1::class.java)) })
+//        bottomNavigationView.menu.getItem(0).setOnMenuItemClickListener({startActivity(Intent(this, Harta_1::class.java))})
+
+
+//        @Override
+//        public boolean onOptionsItemSelected()
+//        bottomNavigationView.menu.getItem (0).setOnClickListener
+//        setOnClickListener({startActivity(Intent(this, Harta_1::class.java)) }
     }
 }
